a227cc2b23bd5bee18393cee6aeff2b2410177d3
Eecodev <mnasser.bbk@gmail.com>
2023-10-19 16:28:14 +0200

    added drawClick function

--- bonus/script.js
+++ bonus/script.js
@@ -3,8 +3,12 @@ const btn = document.querySelector('.btn');
 btn.addEventListener('click', () =>{
 
     const level = document.getElementById('choose').value;
-    console.log(level);
-    
+
+    //numero di quadratini da generare
+    let numSquare = selectLevel(level);
+
+    console.log(numSquare);
+    const squareWidth = Math.sqrt(numSquare);
     //prendo la griglia di gioco
     const playground = document.getElementById('playground');
     //prima di riempire svuoto tutto
@@ -12,30 +16,34 @@ btn.addEventListener('click', () =>{
     //ciclo per stampare gli squares
     for(let i = 0; i < numSquare; i++){
         //genero square
-        let square = drawSquare(i,numSquare);
+        let square = drawSquare(i+1,squareWidth);
         //appendo square al playground
         playground.append(square);
 
     }
 });
 
-function drawSquare(squareIndex,numSquare){
+function drawSquare(content,squareWidth){
     //creo nuovo elemento div
     const square = document.createElement('div');
     //aggiungo square 
     square.classList.add('square');
+    square.style.width = `calc(100% / ${squareWidth})`;
+    square.style.height = square.style.width;
 
     //stampo squares in html
     square.innerHTML = squareIndex + 1;
     //al click...
-    square.addEventListener('click', function(){
-        //lo square diventa aquamarine
-        square.classList.add('active');
-        //e il numero nero
-        square.style.color = 'black';
-        console.log(square.textContent);
-    });
+    square.addEventListener('click');
     return square;
+}
+
+function drawClick(){
+    //lo square diventa aquamarine
+    square.classList.add('active');
+    //e il numero nero
+    square.style.color = 'black';
+    console.log(square.textContent);
 }
 
 function selectLevel(level){
